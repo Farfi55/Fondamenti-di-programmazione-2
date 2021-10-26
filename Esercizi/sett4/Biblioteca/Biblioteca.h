68a0022c5e2f1f8f6b64a347620ef8345faef360
Alessio Farfaglia <djalexy555@gmail.com>
2021-10-26 13:29:46 +0200

    Esercizi: sett4: Biblioteca: aggiunto ridimensionaArray

--- Esercizi/sett4/Biblioteca/Biblioteca.h
+++ Esercizi/sett4/Biblioteca/Biblioteca.h
@@ -6,32 +6,56 @@ class Biblioteca{
 
 private:
 	Libro* libri;
-	unsigned nLibri;
+	unsigned sizeLibri;
+	unsigned capLibri;
 
 	Cliente* clienti;
-	unsigned nClienti;
+	unsigned sizeClienti;
+	unsigned capClienti;
 
 
 	template<class T>
-	void ridimensionaArray(T* &arr, unsigned& size);
+	void ridimensionaArray(T* &arr, unsigned& capacity);
 
 	template<class T>
-	void eliminaElemento(T* arr, unsigned size, unsigned index); 
+	void eliminaElemento(T* arr, unsigned& size, unsigned index); 
 
 
 public:
 	Biblioteca();
 
-	void aggiungiLibro()
-	void RimuoviLibro()
+	void aggiungiLibro(Libro libro);
+	void RimuoviLibro(int index);
 
-	void aggiungiCliente()
-	void RimuoviCliente()
+	void aggiungiCliente(Cliente cliente);
+	void RimuoviCliente(int index);
 
 	Libro* cercaLibro(unsigned long ID);
 	Libro* cercaLibro(string Nome);
+};
+
+template<class T>
+void Biblioteca::ridimensionaArray(T* &arr, unsigned& capacity){
+
+	T* tmp = new T[capacity * 2];
+	
+	for (int i = 0; i < capacity; i++)
+		tmp[i] = arr[i];	
+
+	delete [] arr;
+	arr = tmp;
+	capacity *=2; 
 }
 
+template<class T>
+void Biblioteca::eliminaElemento(T* arr, unsigned& size, unsigned index){
+	for(int i = index; i < size-1; i++);
+		arr[i] = arr[i+1];
+
+	size--;
+}
+
+//void Biblioteca::aggiungiLibro(Libro)
 
 
 #endif
