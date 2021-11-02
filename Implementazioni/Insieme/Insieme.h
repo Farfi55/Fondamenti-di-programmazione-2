6ea7df74bf103cc6e586b3f0469e91f002de0e17
Alessio Farfaglia <djalexy555@gmail.com>
2021-11-02 13:02:17 +0100

    Implementaziooni: Insieme: +commenti descrittivi

--- Implementazioni/Insieme/Insieme.h
+++ Implementazioni/Insieme/Insieme.h
@@ -1,5 +1,3 @@
-
-
 #ifndef INSIEME_H
 #define INSIEME_H
 
@@ -8,7 +6,6 @@
 #include <vector>
 
 using namespace std;
-
 
 template <class T>
 class insieme {
@@ -29,10 +26,8 @@ class insieme {
     insieme<T>& differenceWith(const insieme<T>& set) const;
     insieme<T>& simmetricDifferenceWith(const insieme<T>& set) const;
 
-
     template <class U>
     friend ostream& operator<<(ostream& o, const insieme<U>& set);
-
 };
 
 // template<class T>
@@ -59,9 +54,9 @@ bool insieme<T>::add(const T& e) {
 template <class T>
 bool insieme<T>::remove(const T& e) {
     auto it = find(s.begin(), s.end(), e);
-    if (it == s.end()) 
+    if (it == s.end())
         return false;  // non è stato trovato l'elemento
-    else{
+    else {
         s.erase(it);
         return true;
     }
@@ -72,30 +67,45 @@ bool insieme<T>::isIn(const T& e) const {
     return find(s.begin(), s.end(), e) != s.end();
 }
 
+// paragonabile ad un OR logico
+// esempio utilizzo
+// IN:  insA:   (1,3,4,5)
+//      insB:   (2,3,4)
+// OUT: insOut: (1,2,3,4,5)
 template <class T>
 insieme<T>& insieme<T>::unionTo(const insieme<T>& set) const {
-    if (set.size() > size()) 
-        return set.unionTo(*this);
+    // visto che l'operazione resta invariata
+    // Ottimizzo invertendo l'insieme "this" e l'insieme parametro
+    // quando quest'ultimo è più grande
+    if (set.size() > size()) return set.unionTo(*this);
 
     insieme<T>* tmp = new insieme<T>(set);
 
-    for (int i = 0; i < size(); i++) 
-        tmp->add(s[i]);
+    for (int i = 0; i < size(); i++) tmp->add(s[i]);
 
     return *tmp;
 }
 
-
+// paragonabile ad un AND logico
+// esempio utilizzo
+// IN:  insA:   (1,3,4,5)
+//      insB:   (2,3,4)
+// OUT: insOut: (3,4)
 template <class T>
 insieme<T>& insieme<T>::intersectTo(const insieme<T>& set) const {
     auto* tmp = new insieme<T>();
-    for (int i = 0; i < s.size(); i++){
-        if(find(set.s.begin(), set.s.end(), s[i]) != set.s.end()) tmp->add(s[i]);
+    for (int i = 0; i < s.size(); i++) {
+        if (find(set.s.begin(), set.s.end(), s[i]) != set.s.end())
+            tmp->add(s[i]);
     }
     return *tmp;
 }
 
-
+// paragonabile ad (insA && !insB)
+// esempio utilizzo
+// IN:  insA:   (1,3,4,5)
+//      insB:   (2,3,4)
+// OUT: insOut: (1,5)
 template <class T>
 insieme<T>& insieme<T>::differenceWith(const insieme<T>& set) const {
     insieme<T>* tmp = new insieme<T>(*this);
@@ -108,36 +118,38 @@ insieme<T>& insieme<T>::differenceWith(const insieme<T>& set) const {
     return *tmp;
 }
 
-
+// non richiesto dal prof ma utile comunque
+// funziona come un Exclusive OR logico, applicato agli insiemi
+// esempio utilizzo
+// IN:  insA:   (2,3,4)
+//      insB:   (1,3,4,5)
+// OUT: insOut: (1,2,5)
 template <class T>
 insieme<T>& insieme<T>::simmetricDifferenceWith(const insieme<T>& set) const {
-    if (set.size() > size()) 
-        return set.differenceWith(*this);
-    
+    // visto che l'operazione resta invariata
+    // Ottimizzo invertendo l'insieme "this" e l'insieme parametro
+    // quando quest'ultimo è più grande
+    if (set.size() > size()) return set.differenceWith(*this);
+
     insieme<T>* tmp = new insieme<T>(set);
 
-    for (int i = 0; i < size(); i++){
+    for (int i = 0; i < size(); i++) {
         auto it = find(tmp->s.begin(), tmp->s.end(), s[i]);
-        if (it == tmp->s.end()){ // non trovato
+        if (it == tmp->s.end()) {  // non trovato
             const T e = s[i];
             tmp->s.push_back(e);
-        }
-        else{  // trovato
+        } else {  // trovato
             tmp->s.erase(it);
         }
     }
     return *tmp;
 }
 
-
-
 template <class U>
 ostream& operator<<(ostream& o, const insieme<U>& set) {
     for (int i = 0; i < set.size(); i++) {
         o << set.s[i] << " ";
     }
-
-    o << endl;
     return o;
 }
 
