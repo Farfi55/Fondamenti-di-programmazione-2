691eecec790e777fc3120c78356ad14f8a26341b
Alessio Farfaglia <djalexy555@gmail.com>
2021-11-20 23:30:26 +0100

    Eser 8: aggiunta lezione venerdì

---
+++ Esercizi/sett8/eser_Albero/main.cpp
@@ -0,0 +1,267 @@
+#include <iostream>
+#include <list>
+#include <vector>
+#include <queue>
+#include <tuple>
+#include <string>
+
+#include "../../../Implementazioni/AlberoB/AlberoB.h"
+
+using namespace std;
+
+
+bool piramideSalda(const AlberoB<int>& a, int pesoMassimo);
+
+bool ogniPercorsoRadiceFoglia(const AlberoB<int>& a, int max, int sommaCorrente);
+
+
+bool pariEDispari(const AlberoB<int>& a);
+
+int sommaPath(const AlberoB<int>& a);
+
+int sommaPath(const AlberoB<int>& a, int sommaCorrente);
+
+
+// PER CASA
+
+// restituisce true solo se il numero di foglie con val < 0 
+// è uguale al numero di foglie con val >= 0
+
+bool fogliePosEqFoglieNeg(const alberoB<int> a);
+
+// restituisce true se la somma di ogni coppia di livelli adiacenti non superano il max
+bool sommaLivelliAdiacenti(const alberoB<int> a, int max);
+
+
+/*
+  Supponiamo che ogni percorso dalla radice alla foglia costituisca una parola
+ret true:
+    se ogni parola ha un numero di vocali e consonanti che differiscono al più di uno.
+si supponga che la parola contenga solo lettere che siano minuscole
+
+ */
+bool vocaliEConsonanti(const alberoB<char>& a);
+
+
+
+int main(int argc, char const* argv[])
+{
+
+    // AlberoB<int> a(4);
+
+    // AlberoB<int> b(3);
+    // AlberoB<int> c(2);
+    // a.insFiglio(SIN, b);
+    // a.insFiglio(DES, c);
+
+    // AlberoB<int> d(2);
+    // AlberoB<int> e(1);
+    // b.insFiglio(SIN, d);
+    // b.insFiglio(DES, e);
+
+    // AlberoB<int> f(4);
+    // AlberoB<int> g(2);
+    // c.insFiglio(SIN, f);
+    // c.insFiglio(DES, g);
+
+    // AlberoB<int> h(3);
+    // d.insFiglio(SIN, h);
+
+    AlberoB<int> a(2);
+
+    AlberoB<int> b(3);
+    AlberoB<int> c(4);
+    a.insFiglio(SIN, b);
+    a.insFiglio(DES, c);
+
+    AlberoB<int> d(5);
+    AlberoB<int> e(1);
+    b.insFiglio(SIN, d);
+    b.insFiglio(DES, e);
+
+    AlberoB<int> f(3);
+    c.insFiglio(SIN, f);
+
+
+
+
+    if(ogniPercorsoRadiceFoglia(a, 15, 0)) {
+        std::cout << "nessun percorso supera il massimo" << std::endl;
+    }
+
+    if(pariEDispari(a))
+        std::cout << "Pari e dispari" << std::endl;
+
+
+    cout << sommaPath(a) << endl;
+
+    return 0;
+}
+
+
+typedef AlberoB<int> AlberoBInt;
+
+/*
+Piramide salta = true
+sse la somma dei valori su ogni livello è <= pesoMassimo
+ */
+
+bool piramideSalda(const AlberoB<int>& a, int pesoMassimo) {
+
+    if(a.nullo())
+        return true;
+
+    queue<AlberoBInt> qAlberi;
+    queue<int> qLivelli;
+
+    qAlberi.push(a);
+    qLivelli.push(1);
+
+    int somma = 0;
+    int livelloCorrente = 1;
+
+    while(!qAlberi.empty()) {
+        AlberoBInt temp = qAlberi.front(); // qAlberi.front() restituisce l'oggetto in 
+        //testa alla coda SENZA RIMUOVERLO
+        qAlberi.pop();			 // qAlberi.pop() rimuove l'oggetto in testa alla coda
+
+        // elaborare l'albero temp
+        int livelloTmp = qLivelli.front();
+        qLivelli.pop();
+
+        if(livelloTmp == livelloCorrente)
+            somma += temp.radice();
+        else {
+            if(somma > pesoMassimo) {
+                return false;
+            }
+            livelloCorrente++;
+            somma = temp.radice();
+        }
+
+        if(!temp.figlio(SIN).nullo()) {
+            qAlberi.push(temp.figlio(SIN));
+            qLivelli.push(livelloTmp + 1);
+        }
+        if(!temp.figlio(DES).nullo()) {
+            qAlberi.push(temp.figlio(DES));
+            qLivelli.push(livelloTmp + 1);
+        }
+    }
+
+    if(somma > pesoMassimo) return false;
+    else return true;
+}
+
+
+
+bool ogniPercorsoRadiceFoglia(const AlberoB<int>& a, int max, int sommaCorrente) {
+    if(a.nullo()) return true;
+
+    sommaCorrente += a.radice();
+    if(a.foglia())
+        return sommaCorrente <= max;
+
+    return ogniPercorsoRadiceFoglia(a.figlio(SIN), max, sommaCorrente)
+        && ogniPercorsoRadiceFoglia(a.figlio(DES), max, sommaCorrente);
+}
+
+
+bool ogniPercorsoRadiceFoglia(const AlberoB<int>& a, int max) {
+    if(a.nullo()) return true;
+
+    max -= a.radice();
+    if(a.foglia())
+        return (max >= 0);
+
+    return ogniPercorsoRadiceFoglia(a.figlio(SIN), max)
+        && ogniPercorsoRadiceFoglia(a.figlio(DES), max);
+}
+
+
+
+bool pariEDispari(const AlberoB<int>& a) {
+    if(a.nullo())
+        return true;
+
+    queue<AlberoBInt> qAlberi;
+    queue<int> qLivelli;
+
+    qAlberi.push(a);
+    qLivelli.push(1);
+
+
+    while(!qAlberi.empty()) {
+        AlberoBInt temp = qAlberi.front();
+        qAlberi.pop();
+
+        // elaborare l'albero temp
+        int livelloTmp = qLivelli.front();
+        qLivelli.pop();
+
+        if((temp.radice() + livelloTmp) % 2 != 0)
+            return false;
+
+        if(!temp.figlio(SIN).nullo()) {
+            qAlberi.push(temp.figlio(SIN));
+            qLivelli.push(livelloTmp + 1);
+        }
+        if(!temp.figlio(DES).nullo()) {
+            qAlberi.push(temp.figlio(DES));
+            qLivelli.push(livelloTmp + 1);
+        }
+    }
+
+    return true;
+}
+
+
+int sommaPath(const AlberoB<int>& a) {
+    return sommaPath(a, 0);
+}
+
+
+int sommaPath(const AlberoB<int>& a, int sommaCorrente) {
+    if(a.nullo()) return 0;
+
+    sommaCorrente = sommaCorrente * 10 + a.radice();
+
+    if(a.foglia()) return sommaCorrente;
+    else return sommaPath(a.figlio(SIN), sommaCorrente) +
+        sommaPath(a.figlio(DES), sommaCorrente);
+}
+
+
+AlberoB<int> vecToAlbero(vector<int> vettore) {
+    AlberoB<int> albero(vettore[0]);
+    vector<AlberoB<int>> alberi;
+    alberi.push_back(albero);
+
+    for(int i = 1; i < alberi.size(); i++) {
+        albero<int> newAlbero(vettore[i]);
+        alberi.push_back(newAlbero);
+
+        Direzione dir = (i + 1) % 2;
+        alberi[(i - 1) / 2].insFiglio(dir, newAlbero);
+    }
+
+    return albero;
+
+}
+
+
+/*
+
+
+
+    0 -> 1  ((padre+1) * 2 )-1
+    0 -> 2  ((padre+1) * 2 )
+
+    1->3    ((padre+1) * 2 )-1
+    1->4    ((padre+1) * 2 )
+
+    2->5
+    2->6
+*/
+
+
--- Implementazioni/AlberoB/esempi.cpp
+++ Implementazioni/AlberoB/esempi.cpp
@@ -1,86 +1,88 @@
 template <class T>
-int profondita( AlberoB<T> A )
+int profondita(AlberoB<T> A)
 {
-	if (A.nullo()) return 0; 
-	int p1=profondita(A.figlio(SIN));
-	int p2=profondita(A.figlio(DES));
-	return (p1>p2)? p1+1 : p2+1;
-	
+	if(A.nullo()) return 0;
+	int p1 = profondita(A.figlio(SIN));
+	int p2 = profondita(A.figlio(DES));
+	return (p1 > p2) ? p1 + 1 : p2 + 1;
+
 }
 
 
 template <class T>
-bool bilanciato(AlberoB<T> A )
+bool bilanciato(AlberoB<T> A)
 {
-	if (A.nullo()) return true;
-	int p1=profondita(A.figlio(SIN));
-	int p2=profondita(A.figlio(DES));
-	bool b1=(abs(p1-p2)<=1);
-	bool b2=bilanciato(A.figlio(SIN));
-	bool b3=bilanciato(A.figlio(DES));
-	
-	return b1&&b2&&b3;
+	if(A.nullo()) return true;
+	int p1 = profondita(A.figlio(SIN));
+	int p2 = profondita(A.figlio(DES));
+	bool b1 = (abs(p1 - p2) <= 1);
+	bool b2 = bilanciato(A.figlio(SIN));
+	bool b3 = bilanciato(A.figlio(DES));
+
+	return b1 && b2 && b3;
 }
 
 
 template <class T>
-bool alberoBilanciatoProfondo( AlberoB<T> A, int& p )
-{	if ( A.nullo() ) 
-	{	p = 0;
+bool alberoBilanciatoProfondo(AlberoB<T> A, int& p)
+{
+	if(A.nullo()) {
+		p = 0;
 		return true;
 	}
 	Card p1, p2;
-	bool b1 = alberoBilanciatoProfondo(A.figlio(SIN),p1);
-	bool b2 = alberoBilanciatoProfondo(A.figlio(DES),p2);
-	p = max(p1,p2)+1;
-	return b1 && b2 && (abs(p1-p2) <= 1);
+	bool b1 = alberoBilanciatoProfondo(A.figlio(SIN), p1);
+	bool b2 = alberoBilanciatoProfondo(A.figlio(DES), p2);
+	p = max(p1, p2) + 1;
+	return b1 && b2 && (abs(p1 - p2) <= 1);
 }
 
-template <class T> 
-void visitaSimmetrica( const AlberoB<T>& A, list<T>& L )
-{	if ( !A.nullo() )
-	{	visitaSimmetrica(A.figlio(SIN),L); 	//**
+template <class T>
+void visitaSimmetrica(const AlberoB<T>& A, list<T>& L)
+{
+	if(!A.nullo()) {
+		visitaSimmetrica(A.figlio(SIN), L); 	//**
 		L.push_back(A.radice()); 		//**
-		visitaSimmetrica(A.figlio(DES),L);	//**
+		visitaSimmetrica(A.figlio(DES), L);	//**
 	}
 }
 
 
 void visitaPerLivelli(const AlberoB<int>& a) {
-    if (a.nullo())
-        return;
-    
-    std::queue<AlberoBInt> q;
-    q.push(a);
-    
-    while(!q.empty()) {
-        AlberoBInt temp = q.front(); // q.front() restituisce l'oggetto in 
-//testa alla coda SENZA RIMUOVERLO
-        q.pop();			 // q.pop() rimuove l'oggetto in testa alla coda
-        
-// elaborare l'albero temp
-        std::cout << temp.radice() << ' ';
+	if(a.nullo())
+		return;
 
-        if (!temp.figlio(SIN).nullo())
-            q.push(temp.figlio(SIN));
-        if (!temp.figlio(DES).nullo())
-            q.push(temp.figlio(DES));
-    }
+	std::queue<AlberoBInt> q;
+	q.push(a);
+
+	while(!q.empty()) {
+		AlberoBInt temp = q.front(); // q.front() restituisce l'oggetto in 
+//testa alla coda SENZA RIMUOVERLO
+		q.pop();			 // q.pop() rimuove l'oggetto in testa alla coda
+
+// elaborare l'albero temp
+		std::cout << temp.radice() << ' ';
+
+		if(!temp.figlio(SIN).nullo())
+			q.push(temp.figlio(SIN));
+		if(!temp.figlio(DES).nullo())
+			q.push(temp.figlio(DES));
+	}
 }
 
 
 
 template <class T>
 AlberoB<T> cerca(const AlberoB<T>& a, T v) {
-    if (a.nullo() || a.radice() == v)
-        return a;
-    
-    // a questo punto sono in un sottoalbero che non ha v come valore 
+	if(a.nullo() || a.radice() == v)
+		return a;
+
+	// a questo punto sono in un sottoalbero che non ha v come valore 
 //informativo e non è nullo
-    AlberoB<T> temp = cerca(a.figlio(SIN), v);
-    if (!temp.nullo())
-        return temp;
-    // a questo punto so che nel sottoalbero sinistro non c'era un albero con 
+	AlberoB<T> temp = cerca(a.figlio(SIN), v);
+	if(!temp.nullo())
+		return temp;
+	// a questo punto so che nel sottoalbero sinistro non c'era un albero con 
 // v come valore informativo
-    return cerca(a.figlio(DES), v);
+	return cerca(a.figlio(DES), v);
 }
