9a6edf09570fce31aab82f5d349ad91d08f4dd1d
Alessio Farfaglia <djalexy555@gmail.com>
2021-12-13 15:29:28 +0100

    generatore in vari modi

--- Esercizi/sett10/brute_force_bitstring.cpp
+++ Esercizi/sett10/brute_force_bitstring.cpp
@@ -4,6 +4,41 @@
 #include <list>
 using namespace std;
 
+typedef vector<bool> vectorBool;
+list<vectorBool> genera(int n);
+void genera_ric(int n, vector<bool>& v, list<vectorBool>& l);
+
+vectorBool gen_first(int n);
+void gen_next(vectorBool& prev);
+
+
+
+class Gen {
+private:
+    const int n;
+    vectorBool current;
+    int iter;
+
+public:
+    Gen(int _n) : n(_n), current(n, false), iter(0) {}
+
+    vectorBool& next() {
+        if(iter++ == 0) return current;
+
+        bool carry = true;
+        for(int i = n - 1; carry && i >= 0; --i) {
+            if(!current[i]) carry = false;
+            current[i] = !current[i];
+        }
+        return current;
+    }
+
+    vectorBool getCurrent() const { return current; }
+    void setCurrent(const vectorBool& _current) { current = _current; }
+
+    int getIteration() const { return iter; }
+};
+
 
 int main(int argc, char const* argv[])
 {
@@ -11,7 +46,34 @@ int main(int argc, char const* argv[])
     cin >> n;
 
 
+    Gen gen(n);
+    while(cin.ignore())
+        for(bool digit : gen.next())
+            cout << digit;
 
+
+
+    // vectorBool vbool = gen_first(n);
+    // do {
+    //     gen_next(vbool);
+    //     for(int i = 0; i < vbool.size(); ++i) {
+    //         cout << vbool[i];
+    //     }
+    // } while(cin.get());
+
+
+    // auto l = genera(n);
+    // int x = 0;
+    // for(vectorBool combinazione : l) {
+    //     cout << x++ << '\t';
+
+    //     // print in reverse
+    //     for(int i = combinazione.size() - 1; i >= 0; --i) {
+    //     // for(int i = 0; i < combinazione.size(); ++i) {
+    //         cout << combinazione[i];
+    //     }
+    //     cout << '\n';
+    // }
 
     return 0;
 }
@@ -38,7 +100,6 @@ void generate(vector<int>&values, int max_depth, int depth) {
 
 // 13-12-2021 10:05
 // implementazione prof
-typedef vector<bool> vectorBool;
 list<vectorBool> genera(int n) {
     vector<bool> b(n, false);
     list<vectorBool> l;
@@ -53,12 +114,46 @@ void genera_ric(int n, vector<bool>& v, list<vectorBool>& l) {
         l.push_back(v);
     }
     else {
-        v[n - 1] = 0;
+        v[n - 1] = false;
         genera_ric(n - 1, v, l);
-        v[n - 1] = 1;
+        v[n - 1] = true;
         genera_ric(n - 1, v, l);
     }
 
 }
+
+// USANDO UN GENERATORE
+
+vectorBool gen_first(int n) {
+    return vectorBool(n, false);
+}
+
+
+// non ha nessun controllo
+
+// genera il prossimo 
+void gen_next(vectorBool& prev) {
+    bool carry = true;
+    for(int i = prev.size() - 1; carry && i >= 0; --i) {
+        if(!prev[i]) carry = false;
+        prev[i] = !prev[i];
+    }
+    return;
+}
+
+// o in meno righe
+/*
+void gen_next(vectorBool& prev) {
+    for(int i = prev.size() - 1; i >= 0; --i) {
+        prev[i] = !prev[i];
+        if(prev[i]) return;
+    }
+    return;
+}
+*/
+
+
+// USANDO UNA CLASSE GENERATRICE
+
 
 
