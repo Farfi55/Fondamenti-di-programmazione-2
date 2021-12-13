37cf0e589fe759a19b832c1fedb11d7560d4d5cf
Alessio Farfaglia <djalexy555@gmail.com>
2021-12-13 20:30:10 +0100

    risolto divcoins (ben commentato)

--- Esercizi/sett10/approccio_dinamico/divcoins.cpp
+++ Esercizi/sett10/approccio_dinamico/divcoins.cpp
@@ -4,55 +4,132 @@
 
 using namespace std;
 
+int divcoins_greedy(vector<int>& coins);
+int divcoins(vector<int>& coins);
+
 int main(int argc, char const* argv[])
 {
 
     // 10 10 6 5 1
 
     // 5 3 12 9
-    // 12 9 5 3
 
     vector<int> coins;
 
-    int input;
-    while(true) {
-        cin >> input;
-        if(input == -1) break;
-        coins.push_back(input);
+    {
+        int input;
+        while(true) {
+            cin >> input;
+            if(input == -1) break;
+            coins.push_back(input);
+        }
     }
 
 
-    // 10 4 4 2
+    cout << "differenza usando la tecnica:" << endl;
 
-    // sort(coins.begin(), coins.end(), greater<int>());
+    int diff_golosa = divcoins_greedy(coins);
+    cout << "- golosa:   " << diff_golosa << endl;
 
-    // int difference = 0;
-
-    // for(int i = 0; i < coins.size(); i++) {
-    //     if(difference <= 0)
-    //         difference += coins[i];
-    //     else difference -= coins[i];
-    //     cout << difference << endl;
-    // }
-    const int n = coins.size();
-
-    vector<int> combinazioni;
-
-    // for(int i = 0; i < n; i++)
-    //     combinazioni[i][i] = coins[i];
-
-
-
-    // for(int i = 1; i < n; i++)
-    //     for(int j = 0; j < n - i; j++)
-    //         combinazioni[j][i + j] = combinazioni[j - 1][i + j] + combinazioni[j][i + j - 1];
-
-
-    // for(int i = 0; i < n; i++) {
-    //     for(int j = 0; j < n; j++)
-    //         cout << combinazioni[i][j] << " ";
-    //     cout << endl;
-    // }
+    int diff_dinamica = divcoins(coins);
+    cout << "- dinamica: " << diff_dinamica << endl;
 
     return 0;
 }
+
+
+// IMPLEMENTAZIONE GOLOSA
+
+// fa un sort su coins e poi cerca di minimizzare la differenza
+int divcoins_greedy(vector<int>& coins) {
+    sort(coins.begin(), coins.end(), greater<int>());
+
+    int difference = 0;
+
+    for(int i = 0; i < coins.size(); i++) {
+        if(difference <= 0)
+            difference += coins[i];
+        else difference -= coins[i];
+        // cout << difference << endl;
+    }
+    return difference;
+}
+
+
+// IMPLEMENTAZIONE DINAMICA
+
+int divcoins(vector<int>& coins) {
+
+    // calcoliamo la differenza massima, sommando tutte le monete
+    int somma;
+    for(int coin : coins)
+        somma += coin;
+
+
+    // ci generiamo tutte le somme possibili usando le coins
+    // iniziamo impostandole tutte false
+    vector<bool> somme_possibile(somma, false);
+
+    // tranne 0, che rappresenta non prendere nessuna moneta
+    somme_possibile[0] = true;
+
+    // per ogni moneta 
+    for(int coin : coins)
+        for(int i = somma - 1; i >= 0; --i)
+            if(somme_possibile[i]) { // iteriamo su ogni possibile somma
+                somme_possibile[i + coin] = true;
+                // dalla quale possiamo aggiungere il valore di coin 
+                // e generare un'altra soluzione valida
+                // sulla quale sarà poi possibile aggiungere altre monete,
+                // finchè ne rimangono
+            }
+
+    // la soluzione ottima sarebbe somma_possibile[somma/2],
+    // ma non è detto che sià possibile, quindi iniziando da questo elemento
+    // andiamo avanti finchè non troviamo il primo possibile, 
+    // che sarà la soluzione ottima per questo problema
+    for(int i = somma / 2; i >= 0; --i)
+        if(somme_possibile[i])
+            return somma - (i * 2);
+
+    // non arriva mai qui, visto che somme_possibile[0] è sempre true
+    return somma;
+}
+
+
+/*
+esempio: 1 (somma dispari min: 0)
+
+        10 T : somma
+        9
+        8  T
+        7
+        6  T
+        5  F   <-
+        4  T
+        3
+        2  T
+        1
+        0  T
+
+        10 - 4 - 4 = 2 giusto
+
+esempio: 2 (somma pari min: 1)
+        11 T : somma
+        10
+        9
+        8  T
+        7
+        6  T
+        5  T   <-
+        4
+        3  T
+        2
+        1
+        0  T
+
+    11 - 5 - 5 = 1 giusto
+
+*/
+
+
